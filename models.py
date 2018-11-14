b722fa2471baf20a9e732beeec2167eb763a5c2d
Jason <44716223+Jasonnautilus@users.noreply.github.com>
2018-11-14 16:50:16 -0500

    update CNN, WORK IN PROGRESS

--- models.py
+++ models.py
@@ -20,6 +20,7 @@ class RNN(nn.Module):
         x = F.sigmoid(x)
         return x
 
+
 class FFNN(nn.Module):
     def __init__(self, dim_in, dim_hidden, dim_out, memory=3, num_layers=5):
         super(FFNN, self).__init__()
@@ -40,39 +41,40 @@ class FFNN(nn.Module):
         return x
 
 
+# work in progress
+class CNN(nn.Module):
+    def __init__(self, convolve_time, n_filters, filter_sizes):
+        super(CNN, self).__init__()
 
-# class CNN(nn.Module):
-#     def __init__(self, embedding_dim, vocab, n_filters, filter_sizes):
-#         super(CNN, self).__init__()
-#
-#         ######
-#
-#         # 4.3 YOUR CODE HERE
-#         self.embed = nn.Embedding.from_pretrained(vocab.vectors)
-#         self.conv_a = nn.Conv2d(1, n_filters, (filter_sizes[0], embedding_dim))
-#         self.conv_b = nn.Conv2d(1, n_filters, (filter_sizes[1], embedding_dim))
-#
-#         self.fc = nn.Linear(embedding_dim, 1)
-#
-#         ######
-#
-#
-#     def forward(self, x, lengths=None):
-#         ######
-#
-#         # 4.3 YOUR CODE HERE
-#         x = self.embed(x)
-#         x = x.reshape(-1, 1, x.shape[1], x.shape[2])
-#         x = x.permute(2,1,0,3)
-#
-#         a = F.relu(self.conv_a(x)).squeeze(dim=3)
-#         b = F.relu(self.conv_b(x)).squeeze(dim=3)
-#         a, _ = torch.max(a, 2)
-#         b, _ = torch.max(b, 2)
-#
-#         x = torch.cat((a, b), dim=1)
-#         x = self.fc(x)
-#         x = F.sigmoid(x)
-#         x = x.view(-1)
-#         ######
-#         return x
+        ######
+
+        # Create 2D convolutions over note data and time
+        self.conv_pitch = nn.Conv2d(1, n_filters, (filter_sizes[0], convolve_time))  # convolve over pitch
+        self.conv_octave = nn.Conv2d(1, n_filters, (filter_sizes[1], convolve_time))  # convolve over octave
+        self.conv_rest = nn.Conv1d(1, n_filters, convolve_time)
+
+        self.fc = nn.Linear(convolve_time, 1)
+        self.sigmoid = nn.Sigmoid()
+        ######
+
+    def forward(self, x):  # x is input
+        ######
+
+        # assume you get a batch's worth set of notes, width 24 (pitch+octave+rest), and some length (3-d array)
+        x = x.reshape(-1, 1, x.shape[1], x.shape[2])
+        x = x.permute(2,1,0,3)
+
+        # assume dimensions are (batch num, 1, notes, time). Take convolutions over pitch, octave, and rest
+        x_pitch = F.relu(self.conv_pitch(x[:, :, 0:12, :])).squeeze()
+        x_octave = F.relu(self.conv_octave(x[:, :, 12:23, :])).squeeze()
+        x_rest = F.relu(self.conv_rest(x)).squeeze(x[:, :, 23, :])
+
+        a, _ = torch.max(a, 2)
+        b, _ = torch.max(b, 2)
+
+        x = torch.cat((a, b), dim=1)
+        x = self.fc(x)
+        x = self.sigmoid(x)
+        x = x.view(-1)
+        ######
+        return x
